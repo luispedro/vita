574d6a56f287bf4d239d6a564da5a2dd5def1525
Luis Pedro Coelho <luis@luispedro.org>
2022-05-06 00:49:16 +0200

    BLD Update nix pins

--- nixml.nix
+++ nixml.nix
@@ -1,8 +1,8 @@
 
 with (import (builtins.fetchTarball {
-  name = "nixml-stable-20.09";
-  url = https://github.com/nixos/nixpkgs/archive/0d60b0b10eae7a29abb1cbcd47a764c752b39bd9.tar.gz;
-  sha256 = "067aj1j2zp919gqj8cq749x9byxclb732dxkh1z65bddkca76nnm";
+  name = "nixml-stable-22.05";
+  url = https://github.com/nixos/nixpkgs/archive/5c0c0f0ee8d7b536925580e723a5f03bf6e0b7a4.tar.gz;
+  sha256 = "0v1fbc261by0dfdwysr906garc081swwlyy5x3d7daf98gr5k7yx";
 }) {});
 
 let
